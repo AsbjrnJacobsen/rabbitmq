59fe4ed511faa669d0297778262bf7e2f75a0e04
AsbjrnJacobsen <megetunikemail@gmail.com>
2024-12-29 20:31:56 +0100

    Producer + Consumer Load Balanced

---
+++ Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "admin",
+                Password = "admin"
+            };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var random = new Random();
+
+var messageId = 1;
+
+while (true)
+{
+    var publishingTime = random.Next(1, 4);
+    
+    var message = $"Sending messageId: {messageId++}";
+
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+    Console.WriteLine($"Published message {message}");
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+}
+
